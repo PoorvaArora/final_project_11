cb03f55dba5a549c67418a4c84ee7e8e73c6657b
seancarr19 <seancarr@iu.edu>
2018-10-16 18:50:34 -0400

    display site details

--- public/js/site_turbine_info.js
+++ public/js/site_turbine_info.js
@@ -3,7 +3,8 @@ var agsApp = new Vue({
   data: {
     sitesTurbs:[],
     turbines:[],
-    isTurbineDisplay:false
+    isTurbineDisplay:false,
+    isSiteDisplay:false
   },
   computed: {
   },
@@ -19,12 +20,23 @@ var agsApp = new Vue({
       })
     },
     showTurbineDetails(turbineId){
-      console.log(turbineId);
       fetch('api/turbine.php?turbineId='+turbineId)
       .then( response => response.json() )
       // ^ This is the same as .then( function(response) {return response.json()} )
       .then( json => {agsApp.turbines = json;
         agsApp.isTurbineDisplay = true;
+      })
+      .catch( err => {
+        console.log('TASK FETCH ERROR:');
+        console.log(err);
+      })
+    },
+    showSiteDetails(siteId){
+      fetch('api/site.php?siteId='+siteId)
+      .then( response => response.json() )
+      // ^ This is the same as .then( function(response) {return response.json()} )
+      .then( json => {agsApp.sites = json;
+        agsApp.isSiteDisplay = true;
       })
       .catch( err => {
         console.log('TASK FETCH ERROR:');
