98eac339ba4f0e55f635ba268367eafea1a27224
seancarr19 <seancarr@iu.edu>
2018-10-16 20:07:24 -0400

    added method to call site by id

--- app/models/Site.php
+++ app/models/Site.php
@@ -54,4 +54,29 @@ class Site
     return $arr;
   }
 
+  public static function fetchSiteBySiteId(int $siteId) {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM site WHERE siteId = ?';
+
+    $statement = $db->prepare($sql);
+
+    // 3. Run the query
+    $success = $statement->execute(
+        [$siteId]
+    );
+
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      // 4.a. For each row, make a new work object
+      $siteItem =  new Turbine($row);
+
+      array_push($arr, $siteItem);
+    }
+    return $arr;
+  }
+
 }
