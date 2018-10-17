442220384b03eb76c4ec784cca3b695ca759bd88
hanlou95 <hanlou@iu.edu>
2018-10-17 16:43:15 -0400

    add note php file

--- public/api/note.php
+++ public/api/note.php
@@ -7,11 +7,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   exit;
 }
 
-// 1. Go to the database and get all teams
-$comments = Comment::fetchAll();
+$notesByClientId = Note::fetchNotesByClientId($clientId);
 
 // 2. Convert to JSON
-$json = json_encode($comments, JSON_PRETTY_PRINT);
+$json = json_encode($notesByClientId, JSON_PRETTY_PRINT);
 
 // 3. Print
 header('Content-Type: application/json');
