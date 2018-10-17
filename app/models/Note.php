792caa8c0a12b9ee867404b1ee10128c0995b36f
hanlou95 <hanlou@iu.edu>
2018-10-17 16:28:14 -0400

    added notes

---
+++ app/models/Note.php
@@ -0,0 +1,53 @@
+<?php
+
+class Comment
+{
+  public $id;
+  public $comment;
+
+  public function __construct($data) {
+    $this->id = isset($data['id']) ? intval($data['id']) : null;
+    $this->comment = $data['comment'];
+  }
+
+  public function create() {
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    $sql = 'INSERT INTO Comment (id,comment)
+            VALUES (?,?)';
+
+    $statement = $db->prepare($sql);
+
+    $success = $statement->execute([
+      $this->id,
+      $this->comment
+    ]);
+
+    if (!$success) {
+      // TODO: Better error handling
+      die('SQL error');
+    }
+    $this->id = $db->lastInsertId();
+  }
+
+  public static function fetchAll() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM Comment';
+    $statement = $db->prepare($sql);
+
+    // 3. Run the query
+    $success = $statement->execute();
+
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      $theComment =  new Comment($row);
+      array_push($arr, $theComment);
+    }
+
+    return $arr;
+  }
+
+}
