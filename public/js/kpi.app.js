998e7870aa0b50c135dedd25e96c9a284359be8b
PoorvaArora <pooarora@iu.edu>
2018-10-20 14:55:52 -0400

    kpi charts

---
+++ public/js/kpi.app.js
@@ -0,0 +1,78 @@
+var agsApp = new Vue({
+  el: '#agsKPI',
+  data: {
+    sensors:[]
+  },
+  computed: {
+  },
+  methods: {
+    fetchSensorTimeSeries () {
+      fetch('api/sensorTimeSeries.php')
+      .then( response => response.json() )
+      // ^ This is the same as .then( function(response) {return response.json()} )
+      .then( json => {agsApp.sonsors = json})
+      .catch( err => {
+        console.log('SENSOR FETCH ERROR:');
+        console.log(err);
+      })
+    },
+    buildEffortChart() {
+      Highcharts.chart('effortChart', {
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Cumulative Effort'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Trips'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+            series: [{
+                type: 'area',
+                name: 'Trips',
+                data:  this.sensors.map( entry =>
+                  [entry.dataCollectedDate, entry.trips]
+                ) //Expects [ [date1, val1], [date2, val2], [] ]
+            }]
+        });
+    }
+  },
+  created () {
+    this.fetchSensorTimeSeries();
+    this.buildEffortChart();
+  }
+})
