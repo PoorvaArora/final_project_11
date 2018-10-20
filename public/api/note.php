98512d7c8ccdc8f89edb1dd7c533f1447e173e19
hanlou95 <hanlou@iu.edu>
2018-10-20 13:42:26 -0400

    fetch by client id

--- public/api/note.php
+++ public/api/note.php
@@ -7,15 +7,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   exit;
 }
 
-// 1. Go to the database and get all client
-$note = Note::fetchAll();
-
-// 2. Convert to JSON u
-$json = json_encode($note, JSON_PRETTY_PRINT);
-
-// 3. Print
-header('Content-Type: application/json');
-echo $json;
+// // 1. Go to the database and get all client
+// $note = Note::fetchAll();
+//
+// // 2. Convert to JSON u
+// $json = json_encode($note, JSON_PRETTY_PRINT);
+//
+// // 3. Print
+// header('Content-Type: application/json');
+// echo $json;
 
 
 $notesByClientId = Note::fetchNotesByClientId($clientId);
